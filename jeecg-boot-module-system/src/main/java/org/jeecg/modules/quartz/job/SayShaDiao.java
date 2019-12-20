15b05fa28d2125eacd8e165ec99b557ebb5d2775
Administrator <178697490@qq.com>
2019-12-20 12:17:00 +0800

    111

---
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/quartz/job/SayShaDiao.java
@@ -0,0 +1,28 @@
+package org.jeecg.modules.quartz.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jeecg.common.util.DateUtils;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+/**
+ * @author Administrator
+ */
+@Slf4j
+public class SayShaDiao implements Job {
+
+    /**
+     * 若参数变量名修改 QuartzJobController中也需对应修改
+     */
+    private String parameter;
+
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+        log.info(String.format(" %s! 你个傻屌 !   时间:" + DateUtils.now(), this.parameter));
+    }
+}
